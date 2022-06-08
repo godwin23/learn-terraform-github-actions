9315e17ebc8b079c970530671c8becef7384689b
godwin23 <code.gost@gmail.com>
2022-06-08 13:50:24 +0300

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "az-ter-org"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "demo-ter-github-actions"
     }
   }
 }
